4838034b2734158d0fa0054edc8662a7b314073a
kmatthews123 <kmatt999@gmail.com>
2025-02-21 18:10:34 -0800

    standardizing antenna code between nodes

--- antenna-code/Zeus/gps/checkpynmeagps.py
+++
@@ -1,9 +0,0 @@
-from serial import Serial
-from pynmeagps import NMEAReader  # type: ignore
-
-with Serial('/dev/ttyS0', 9600, timeout=3) as stream:
-    nmr = NMEAReader(stream)
-    while True:
-        raw_data, parsed_data = nmr.read()
-        print(f"RAW: {raw_data}")
-        print(f"PARSED: {parsed_data}")
